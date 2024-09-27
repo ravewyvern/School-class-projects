3ecfd80631e5d761d9e4cb230f5127589db39407
Rave Wyvern <87492063+ravewyvern@users.noreply.github.com>
2024-09-27 00:47:15 +0000

    did some stuff

---
+++ sep 26th/AroundTheWorld.java
@@ -0,0 +1,20 @@
+public class AroundTheWorld 
+{
+    public static void main(string[] args)
+    {
+        repeatEightTimes();
+        System.out.println();
+        repeatSixteenTimems();
+        System.out.println();
+    }
+
+
+    public static void aroundTheWorld() {
+        System.out.println("Around the world, around the world"));
+    }
+
+    public static void repeatEightTimes()
+    {
+        
+    }
+}
